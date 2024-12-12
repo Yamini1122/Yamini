a7a1cfa200c54bc772b29ebe4e0530921ce15e63
Yamini1122 <reddy.yamini2003@gmail.com>
2024-12-12 11:48:17 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -2,7 +2,7 @@ class helloworld
 {
     public static void main(String []args)
     {
-        System.out.println("My First Java Program.");
+        System.out.println("hi friends.");
         System.out.println("My First Java Program.");
         System.out.println("My First Java Program.");
         
